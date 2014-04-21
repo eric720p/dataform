17c35666808500beee0c3bd23afbc08190378158
Dustin Larimer <dustinlarimer@gmail.com>
2014-04-21 15:07:16 -0700

    Build/test structure

---
+++ test/test-spec.js
@@ -0,0 +1,17 @@
+var expect = chai.expect;
+
+describe("dataform", function() {
+
+  beforeEach(function() {
+    console.log("ready");
+  });
+
+  describe("first test", function() {
+
+    it("should pass", function(){
+      expect(true).to.be.ok
+    });
+
+  });
+
+});
